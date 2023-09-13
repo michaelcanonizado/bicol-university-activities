dbbc5244af328ffb41dad2edddc343a09afff166
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-09-13 22:38:39 +0800

    changed perimeter of circle to circumference

--- 0101-computer-programming-1/pe-002/circle.c
+++ 0101-computer-programming-1/pe-002/circle.c
@@ -4,12 +4,12 @@
 int main(void) {
     // Initialize the variables where inputs are Integers and products are Floats.
     int r;
-    float A, P;
+    float A, C;
     // Initialize the constant PI
     const float PI = 3.14159265;
 
     // Descriptive prompt to indicate to the user what is about to happen.
-    printf("\nLet's calculate the Area and Perimeter of your circle!\n");
+    printf("\nLet's calculate the Area and Circumference of your circle!\n");
 
     // Prompt the user for radius input.
     printf("\nWhat is the radius?:");
@@ -18,12 +18,12 @@ int main(void) {
     if (r > 0) {
       // Calculate the area
       A = PI * (r * r);
-      // Calculate the perimeter
-      P = 2 * PI * r;
+      // Calculate the circumference
+      C = 2 * PI * r;
 
       // Return the results to the user
       printf("\nArea: %f units squared", A);
-      printf("\nPerimeter: %f units\n\n", P);
+      printf("\nCircumference: %f units\n\n", C);
 
       return 0;
     } else {
