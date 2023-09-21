c36863310f8b5f0eb21e8283ba8992337bb2ded5
unknown <michaelxaviercanonizado@gmail.com>
2023-09-21 11:15:33 +0800

    added basic program

--- 0101-computer-programming-1/pe-003/app.c
+++ 0101-computer-programming-1/pe-003/app.c
@@ -10,6 +10,38 @@ int main() {
     printf("\nInput name of student: ");
     scanf("%s", name);
 
-    printf("name: %s", name);
+    printf("\nPlease input five pairs of integer values representing the total number of points and score of Juan's quizzes. ");
+    
+    printf("\nInput Quiz 1 : ");
+    scanf("%d %d", &q1total, &q1score);
+    printf("Input Quiz 2 : ");
+    scanf("%d %d", &q2total, &q2score);
+    printf("Input Quiz 3 : ");
+    scanf("%d %d", &q3total, &q3score);
+    printf("Input Quiz 4 : ");
+    scanf("%d %d", &q4total, &q4score);    
+    printf("Input Quiz 5 : ");
+    scanf("%d %d", &q5total, &q5score);
+
+    q1rating = ((float)q1score / q1total) * 100;
+    q2rating = ((float)q2score / q2total) * 100;
+    q3rating = ((float)q3score / q3total) * 100;
+    q4rating = ((float)q4score / q4total) * 100;
+    q5rating = ((float)q5score / q5total) * 100;
+
+    average = (q1rating + q2rating + q3rating + q4rating + q5rating) / 5.0;
+
+    printf("\n----------------------------------");
+    printf("\n|%6s |%6s |%6s |%6s  |", "Quiz#", "SCORE", "TOTAL", "RATE");
+    printf("\n----------------------------------");
+    
+    printf("\n|%-6d |%6d |%6d |%6.2f%% |", 1, q1score, q1total, q1rating);
+    printf("\n|%-6d |%6d |%6d |%6.2f%% |", 2, q2score, q2total, q2rating);
+    printf("\n|%-6d |%6d |%6d |%6.2f%% |", 3, q3score, q3total, q3rating);
+    printf("\n|%-6d |%6d |%6d |%6.2f%% |", 4, q4score, q4total, q4rating);
+    printf("\n|%-6d |%6d |%6d |%6.2f%% |", 5, q5score, q5total, q5rating);
+
+    printf("\n\nQuiz Average: %.2f%%", average);
+
     return 0;
 }
