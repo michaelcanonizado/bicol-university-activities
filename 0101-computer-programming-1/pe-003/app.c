f10374af451285520b62f66b357efb248300d787
unknown <michaelxaviercanonizado@gmail.com>
2023-09-21 10:47:18 +0800

    initialized file

---
+++ 0101-computer-programming-1/pe-003/app.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char name[40];
+    int q1score, q2score, q3score, q4score, q5score;
+    int q1total, q2total, q3total, q4total, q5total;
+    float q1rating, q2rating, q3rating, q4rating, q5rating;
+    float average;
+
+    printf("\nInput name of student: ");
+    scanf("%s", name);
+
+    printf("name: %s", name);
+    return 0;
+}
