0f775edd3c9eb31d01f343454e3e82ce8c63d815
unknown <michaelxaviercanonizado@gmail.com>
2023-09-25 11:21:13 +0800

    added lecture lad

---
+++ 0101_computer-programming-1/le-007/fileRW.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void) {
+    int num1, num2;
+
+    FILE *inputFile;
+    FILE *outputFile;
+    inputFile = fopen("input.txt", "r");
+    outputFile = fopen("output.txt", "wt");
+
+    if(inputFile == NULL) {
+        printf("Input file not found");
+        return 1;
+    }
+
+    printf("Reading from index.txt...\n");
+    fscanf(inputFile, "%d %d", &num1, &num2);
+
+    printf("\nReader values: %d %d\nValue when added: %d", num1, num2, num1+num2);
+
+    printf("\nWriting to output.txt...");
+    fprintf(outputFile, "Total: %d\n", num1+num2);
+    printf("\nSuccessfully written to output.txt...");
+
+    printf("\n\nTask complete...");
+
+
+
+
+    fclose(inputFile);
+    fclose(outputFile);
+
+    return 0;
+}
+
