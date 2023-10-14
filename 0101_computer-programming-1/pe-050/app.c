b0e2407c68f9f854933f51506e80c75cc6124d57
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-10-14 20:33:59 +0800

    added user prompt function

--- 0101_computer-programming-1/pe-050/app.c
+++ 0101_computer-programming-1/pe-050/app.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    printf("Kumusta Mundo");
+    // Declare Variables
+    float gradeQuizzes, gradeMidterms, gradeFinals, gradeProject, gradeRecitation;
+
+    // Get grades from user
+    printf("Enter grade for Quizzes: ");
+    scanf("%f", &gradeQuizzes);
+
+    printf("Enter grade for Midterm exam: ");
+    scanf("%f", &gradeMidterms);
+
+    printf("Enter grade for Final exam: ");
+    scanf("%f", &gradeFinals);
+
+    printf("Enter grade for Project: ");
+    scanf("%f", &gradeProject);
+
+    printf("Enter grade for Recitation: ");
+    scanf("%f", &gradeRecitation);
+
+    // Calculate
+
+    // Output
+    
     return 0;
 }
