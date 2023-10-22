263a71153ece7fbd0d3a8666e0b51e95dec8e6b3
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-10-22 19:40:51 +0800

    added input validation

--- 0101_computer-programming-1/pe-060/app.c
+++ 0101_computer-programming-1/pe-060/app.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    char letter;
+    char userCharInput;
 
     printf("ALPHABET SONG\n");
-    printf("Enter a letter: ");
-    scanf("%c", &letter);
+    printf("Enter a userCharInput: ");
+    scanf("%c", &userCharInput);
+
+    // Guard clause to check if input is invalid (not an uppercase). If invlaid, immediately throw an error and exit program.
+    if ((userCharInput < 'A' || userCharInput > 'Z') && (userCharInput < 'a' || userCharInput > 'z')) {
+        printf("\n\t\'%c\' - Invalid Character", userCharInput);
+        return 1;
+    }
+    
+    // Check if input is a lowercase alphabet. If true, turn input to upper case by manipulating the ASCII value.
+    if (userCharInput >= 'a' && userCharInput <= 'z') {
+        userCharInput = userCharInput - 32;
+        printf("\nINPUT: %c", userCharInput);
+        printf("\nLOWERCASE\n\n");
+        return 0;
+    }
+
+    
 
     return 0;
 }
