4fdad63043a84e4c708a523b0ed6a9549476c202
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-11-08 01:11:31 +0800

    added wage rate logic

--- 0101_computer-programming-1/pe-070/app.c
+++ 0101_computer-programming-1/pe-070/app.c
@@ -1,16 +1,50 @@
 #include <stdio.h>
 
 int main(void) {
-    float num1, num2;
+    const float rate1 = 1.0;
+    const float rate2 = 1.5;
+    const float rate3 = 2.0;
+
+    float centavos, pesos, hour, payInPhp, totalPay;
 
     while(1) {
         printf("Please input a pair of integers (ctrl-D to end): ");
-        if (scanf("%f %f", &num1, &num2) == EOF) {
+        if (scanf("%f %f", &centavos, &hour) == EOF) {
             break;
         }
 
-        printf("Pay at %f centavos/hr for %f hours is %f\n\n", num1, num2, num1 + num2);
+        pesos = centavos / 100;
+
+        if (hour <= 40) {
+            payInPhp = (pesos * rate1) * hour;
+            totalPay += payInPhp;
+        } else if (hour >= 41 && hour <= 60) {
+            hour -= 40;
+            
+            payInPhp += pesos * 40;
+
+            payInPhp += (pesos * rate2) * hour;
+
+            totalPay += payInPhp;
+
+        } else if (hour > 60) {
+            hour -= 60;
+
+            payInPhp += pesos * 40;
+
+            payInPhp += (pesos * rate2) * 20;
+
+            payInPhp += (pesos * rate3) * hour;
+
+            totalPay += payInPhp;
+        }
+
+        printf("Pay at %.0f centavos/hr for %.0f hours is %.2f pesos\n\n", centavos, hour, payInPhp);
+        
+        payInPhp = 0;
     }
+
+    // Print to txt here
 
     return 0;
 }
