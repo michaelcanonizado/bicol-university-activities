43f352903f8f49120f34a905415cb8d16f72cfbc
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-11-07 22:54:27 +0800

    added while checking for EOF

--- 0101_computer-programming-1/pe-070/app.c
+++ 0101_computer-programming-1/pe-070/app.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello World");
+    float num1, num2;
+
+    while(1) {
+        printf("Please input a pair of integers (ctrl-D to end): ");
+        if (scanf("%f %f", &num1, &num2) == EOF) {
+            break;
+        }
+
+        printf("Pay at %f centavos/hr for %f hours is %f\n\n", num1, num2, num1 + num2);
+    }
+
     return 0;
 }
