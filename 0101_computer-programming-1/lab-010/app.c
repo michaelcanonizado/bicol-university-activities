5b13165425a6196fabadb7c80aabea0139480da1
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-11-14 22:19:54 +0800

    added lecture 11 problem 6 solution

--- 0101_computer-programming-1/lab-010/app.c
+++ 0101_computer-programming-1/lab-010/app.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
-int main(void) [
+int main(void) {
+    // Initialize Varablez
+    int integer, total;
+
+    // Prompt user
+    printf("Factorial Calculator\n");
+    printf("Enter a positive integer: ");
+    scanf("%d", &integer);
+
+    // We need to set the total's value to the first value of the integer times the second value. This allows it to not be kept inside the for loop. You can do this expression inside the for loop but you need to add a condition.
+    total = integer * (integer-1);
+
+    // Loop through the remaining values needed to be evaluated.
+    // Set i's value to the third value of the integer, and start evaluating from there as the first 2 values has already been stored inside the total variable
+    for(int i = integer-2; i >= 1; i--) {
+        total = total * i;
+    }
+
+    // Output results
+    printf("The factorial of 5 is %d\n", total);
 
     return 1;
-]
+}
