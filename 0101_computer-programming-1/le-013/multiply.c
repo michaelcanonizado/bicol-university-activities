71ac4d04eeaa446663656ae545058882e6d787b8
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-11-21 21:57:00 +0800

    renamed mult.c to multiply.c

---
+++ 0101_computer-programming-1/le-013/multiply.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int multiply(int num) {
+    //
+}
+
+int main(void) {
+    int integer;
+    unsigned long long int result; 
+
+    printf("Please enter a positive integer: ");
+    scanf("%d", &integer);
+
+    result = multiply(integer);
+
+    printf("Factorial of %d is: %llu\n", integer, result);
+}
