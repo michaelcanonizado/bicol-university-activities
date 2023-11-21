0fb9ee4322d03811f975d9f15e780a13fb0dbe21
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-11-21 22:07:03 +0800

    added multipy.c solution using recursion

--- 0101_computer-programming-1/le-013/multiply.c
+++ 0101_computer-programming-1/le-013/multiply.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-int multiply(int num) {
-    
+int multiply(int num1,int num2) {
+    if (num2 == 1) {
+        return num1;
+    } else {
+        return num1 + multiply(num1, num2-1);
+    }
 }
 
 int main(void) {
-    int integer;
+    int integer1, integer2;
     unsigned long long int result; 
 
-    printf("Please enter a positive integer: ");
-    scanf("%d", &integer);
+    printf("Please enter two positive integer: ");
+    scanf("%d %d", &integer1, &integer2);
 
-    result = multiply(integer);
+    result = multiply(integer1, integer2);
 
-    printf("Square of %d is: %llu\n", integer, result);
+    printf("\n%d times %d is: %llu\n", integer1,integer2, result);
 }
