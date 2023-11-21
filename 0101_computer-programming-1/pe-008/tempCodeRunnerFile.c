48b87a44aaa46699e3c184ff5ba3ce8c5127640a
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-11-21 21:35:59 +0800

    copied and renamed app.c to required format

--- 0101_computer-programming-1/pe-008/tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-printf("\nEnter a positive integer(Ctrl-Z to end): ");
-        
-        // if (scanf("%d", &integer) == EOF) {
-        //     printf(" End of program. Thanks!\n\n");
-        //     break;
-        // }
