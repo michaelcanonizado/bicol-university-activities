a022232f7cf6f5e9b994f25260072f5257fc7af8
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-02-07 13:39:02 +0800

    feat: add prompt to ask user for size of array and array values

--- 0103_computer-programming-2/pe-002/index.c
+++ 0103_computer-programming-2/pe-002/index.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+#define MAX_ARRAY_SIZE 1000
+
 int main(void) {
-    printf("Hello, world!");
+    int sizeOfArray;
+    float array[MAX_ARRAY_SIZE];
+
+    printf("\nEnter the size of the array: ");
+    scanf("%d", &sizeOfArray);
+    printf("Enter the array elements: ");
+    for (int i = 0; i < sizeOfArray; i++) {
+        scanf("%f", &array[i]);
+    }
+
+    printf("\nEntered values: ");
+    for (int i = 0; i < sizeOfArray; i++) {
+        printf("%f, ", array[i]);
+    }
 
     return 0;
 }
