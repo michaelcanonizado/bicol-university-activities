9d34dd0cf02da2b5b805f430290e3f3cf5a1949c
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-02-11 18:07:16 +0800

    feat: read values from matrix.in

--- 0103_computer-programming-2/pe-003/index.c
+++ 0103_computer-programming-2/pe-003/index.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 
+#define MAX_MATRIX_ROWS 100
+#define MAX_MATRIX_COLS 100
+
 int main (void) {
-    printf("Hello World");
+    int rows, cols;
+    int matrix[MAX_MATRIX_ROWS][MAX_MATRIX_COLS];
+
+    FILE *pInputFile;
+    pInputFile = fopen("matrix.in", "r");
+
+    if (pInputFile == NULL) {
+        printf("matrix.in not found...");
+        return 1;
+    }
+
+    fscanf(pInputFile, "%d %d", &rows, &cols);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            fscanf(pInputFile, "%d", &matrix[i][j]);
+        }
+    }
+
+    fclose(pInputFile);
+
+    printf("\nRows: %d\nCols: %d\n\n", rows, cols);
+    printf("Matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
---
+++ 0103_computer-programming-2/pe-003/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+ int rows, cols;
+
+    printf("\n\nTEST\n\n");
+    int matrix[MAX_MATRIX_ROWS][MAX_MATRIX_COLS];
