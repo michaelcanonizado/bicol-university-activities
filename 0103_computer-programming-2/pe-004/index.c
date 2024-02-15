002f5d2e18ee45fe5ac7a833fd61e19698f22825
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-02-15 20:58:06 +0800

    feat: convert strings to lowercase before loading into the array using a buffer

--- 0103_computer-programming-2/pe-004/index.c
+++ 0103_computer-programming-2/pe-004/index.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 #define MAX_STR_LEN 50
 #define MAX_NUM_WORDS 100
 
 int main(void) {
-    char singularString[MAX_NUM_WORDS][MAX_STR_LEN];
+    char singularStrings[MAX_NUM_WORDS][MAX_STR_LEN], strBuffer[MAX_STR_LEN];
     int singularStringsCount = 0;
+    
     FILE *pInputFile;
     pInputFile = fopen("singular.in", "r");
 
@@ -14,14 +17,20 @@ int main(void) {
         return 1;
     }
 
-    while(fscanf(pInputFile, "%s", singularString[singularStringsCount]) != EOF) {
+    while(fscanf(pInputFile, "%s", strBuffer) != EOF) {
+        for(int i = 0; strBuffer[i] != '\0'; i++) {
+            strBuffer[i] = tolower(strBuffer[i]);
+        }
+
+        strcpy(singularStrings[singularStringsCount],strBuffer);
+
         singularStringsCount++;
     }
 
     fclose(pInputFile);
 
     for (int i = 0; i < singularStringsCount; i++) {
-        printf("\n%s", singularString[i]);
+        printf("\n%s", singularStrings[i]);
     }
 
     return 0;
