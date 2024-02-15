c0e262fe684a4bec2c8e143a8dcd1b39c12aa683
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-02-16 00:35:57 +0800

    add toPlural() converting an array of singular trings to an array of plural strings

--- 0103_computer-programming-2/pe-004/index.c
+++ 0103_computer-programming-2/pe-004/index.c
@@ -5,10 +5,56 @@
 #define MAX_STR_LEN 50
 #define MAX_NUM_WORDS 100
 
-int main(void) {
-    char singularStrings[MAX_NUM_WORDS][MAX_STR_LEN], strBuffer[MAX_STR_LEN];
-    int singularStringsCount = 0;
+void toPlural(char pluralStrings[MAX_NUM_WORDS][MAX_STR_LEN], char singularStrings[MAX_NUM_WORDS][MAX_STR_LEN], int array_length) {
     
+    for (int i = 0; i <= array_length; i++) {
+
+        int stringLength = strlen(singularStrings[i]);
+        strcpy(pluralStrings[i], singularStrings[i]);
+
+        if (
+            singularStrings[i][stringLength - 1] == 's' ||
+            singularStrings[i][stringLength - 1] == 'x' ||
+            singularStrings[i][stringLength - 1] == 'z' ||
+            (singularStrings[i][stringLength - 2] == 's' && singularStrings[i][stringLength -1] == 'h') ||
+            (singularStrings[i][stringLength - 2] == 'c' && singularStrings[i][stringLength -1] == 'h')
+            ) {
+
+            strcat(pluralStrings[i], "es");
+
+        } else if (singularStrings[i][stringLength - 1] == 'y') {
+
+            if (
+                singularStrings[i][stringLength - 2] == 'a' || 
+                singularStrings[i][stringLength - 2] == 'e' || 
+                singularStrings[i][stringLength - 2] == 'i' || 
+                singularStrings[i][stringLength - 2] == 'o' || 
+                singularStrings[i][stringLength - 2] == 'u') {
+
+                strcat(pluralStrings[i], "s");
+
+            } else {
+
+                pluralStrings[i][stringLength - 1] = 'i';
+                strcat(pluralStrings[i], "es");
+
+            }
+
+        } else {
+
+            strcat(pluralStrings[i], "s");
+
+        }
+
+
+    }
+
+}
+
+int main(void) {
+    char singularStrings[MAX_NUM_WORDS][MAX_STR_LEN], pluralStrings[MAX_NUM_WORDS][MAX_STR_LEN], strBuffer[MAX_STR_LEN];
+    int singularStringsCount = 0;
+
     FILE *pInputFile;
     pInputFile = fopen("singular.in", "r");
 
@@ -29,9 +75,13 @@ int main(void) {
 
     fclose(pInputFile);
 
+    toPlural(pluralStrings, singularStrings, singularStringsCount);
+
+    printf("\n");
     for (int i = 0; i < singularStringsCount; i++) {
-        printf("\n%s", singularStrings[i]);
+        printf("\n%s -> %s", singularStrings[i], pluralStrings[i]);
     }
+    printf("\n\n");
 
     return 0;
 }
