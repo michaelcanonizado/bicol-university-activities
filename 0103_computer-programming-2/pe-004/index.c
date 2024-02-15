2dff86a9cb21cdc8e05a9cee6097af67574be883
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-02-15 20:37:05 +0800

    feat: read singular words from singular.in

--- 0103_computer-programming-2/pe-004/index.c
+++ 0103_computer-programming-2/pe-004/index.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello World");
+    char singularString[100][100];
+    int singularStringsCount = 0;
+    FILE *pInputFile;
+    pInputFile = fopen("singular.in", "r");
+
+    if (pInputFile == NULL) {
+        printf("Error! singular.in file not found!");
+        return 1;
+    }
+
+    while(fscanf(pInputFile, "%s", singularString[singularStringsCount]) != EOF) {
+        singularStringsCount++;
+    }
+
+    fclose(pInputFile);
+
+    for (int i = 0; i < singularStringsCount; i++) {
+        printf("\n%s", singularString[i]);
+    }
 
     return 0;
 }
