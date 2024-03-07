0e96aacc08b1e80b28c0932bdc326975d8a76c4e
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-03-07 17:09:16 +0800

    feat: scan strings in strings.txt

--- 0103_computer-programming-2/pe-005/index.c
+++ 0103_computer-programming-2/pe-005/index.c
@@ -2,7 +2,27 @@
 
 int main(void) {
 
-    printf("Hello World!\n");
+    FILE *pInputFile;
+    pInputFile = fopen("strings.txt", "r");
+
+    if (pInputFile == NULL) {
+        printf("Error! string.txt is not found!\n");
+        return 1;
+    }
+
+    char strings[100];
+    char stringBuffer[100];
+    int stringsCount = 0;
+
+    fscanf(pInputFile, "%d", &stringsCount);
+
+    while(fscanf(pInputFile, "%s", stringBuffer) != EOF) {
+        printf("%s\n", stringBuffer);
+    }
+
+    printf("\n\nScanned %d strings!", stringsCount);
+
+    fclose(pInputFile);
 
     return 0;
 }
