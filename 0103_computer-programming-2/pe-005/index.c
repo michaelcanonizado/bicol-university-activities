67a714dc4ba213f8cda7d91d9e661e60bb8ad42d
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-03-09 21:19:24 +0800

    fix: converted array of char array (matrix) that stores the strings to an array of pointers
    used another array (matrix) to store the strings so the array of pointers can point to it. This is done because malloc is yet to be dicussed undicussed topics are not allowed to be used.

--- 0103_computer-programming-2/pe-005/index.c
+++ 0103_computer-programming-2/pe-005/index.c
@@ -5,6 +5,10 @@
 #define MAX_STRING_LENGTH 100
 #define MAX_NUM_OF_STRINGS 100
 
+void toLowerStrings(char *stringsArr[], int stringsCount) {
+
+}
+
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
         printf("\nPlease specify the input file and output file names when executing the program...\n\n");
@@ -25,22 +29,24 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    char unsortedStrings[MAX_NUM_OF_STRINGS][MAX_STRING_LENGTH];
-    char sortedStrings[MAX_NUM_OF_STRINGS][MAX_STRING_LENGTH];
+    char *stringsPointerArray[MAX_NUM_OF_STRINGS];
+    char stringsStorage[MAX_NUM_OF_STRINGS][MAX_STRING_LENGTH];
     int stringsCount = 0, tempCount = 0;
     char stringBuffer[100];
 
     fscanf(pInputFile, "%d", &stringsCount);
 
-    while(fscanf(pInputFile, "%s", stringBuffer) != EOF) {
-        for(int i = 0; i < strlen(stringBuffer); i++) {
-            unsortedStrings[tempCount][i] = tolower(stringBuffer[i]);
-        }
+    while(fscanf(pInputFile, "%s", stringsStorage[tempCount]) != EOF) {
+        stringsPointerArray[tempCount] = &stringsStorage[tempCount][0];
         tempCount++;
     }
 
     for(int i = 0; i < stringsCount; i++) {
-        printf("string: %s\n", unsortedStrings[i]);
+        printf("string: %-15s - address: %10p\n", stringsStorage[i], &stringsStorage[i]);
+    }
+    printf("\n");
+    for(int i = 0; i < stringsCount; i++) {
+        printf("string: %-15s - address: %10p\n", stringsPointerArray[i], stringsPointerArray[i]);
     }
 
     printf("\n\nScanned %d strings!", stringsCount);
