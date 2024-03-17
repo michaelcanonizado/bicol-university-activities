fba1652d6c842558c225ba94392d1ab109bab539
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-03-17 16:33:06 +0800

    feat: open input and output files

--- 0103_computer-programming-2/pe-006/index.c
+++ 0103_computer-programming-2/pe-006/index.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
     
-    printf("Hello World!");
+    if (argc < 3) {
+        printf("\nPlease specify the input file and output file names when executing the program...\n\n");
+        return 1;
+    }
+
+    FILE *pInputFile = NULL;
+    FILE *pOutputFile = NULL;    
+    pInputFile = fopen(argv[1], "r");
+    pOutputFile = fopen(argv[2], "w");
+
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+
+    fclose(pInputFile);
+    fclose(pOutputFile);
 
     return 0;
 }
