4fd6bb4925d70a17cab7129f08b33c8e3e50548e
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-03-17 21:20:03 +0800

    feat: read students from students.txt input file

--- 0103_computer-programming-2/pe-006/index.c
+++ 0103_computer-programming-2/pe-006/index.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define MAX_NAME_LENGTH 100
+#define MAX_NUM_OF_STUDENTS 100
+
 int main(int argc, char *argv[]) {
     
     if (argc < 3) {
@@ -12,9 +15,34 @@ int main(int argc, char *argv[]) {
     pInputFile = fopen(argv[1], "r");
     pOutputFile = fopen(argv[2], "w");
 
-    for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
+    typedef struct {
+        char name[MAX_NAME_LENGTH];
+        int id;
+        int age;
+        char gender;
+    } Student;
+
+    int studentsCount = 0, arrayIndex = 0;
+    Student studentsArray[MAX_NUM_OF_STUDENTS];
+
+    fscanf(pInputFile, "%d", &studentsCount);
+
+    while(1) {
+        Student student;
+
+        if (fscanf(pInputFile, "%s %d %d %c", student.name, &student.id, &student.age, &student.gender) == EOF) {
+            break;
+        }
+
+        studentsArray[arrayIndex] = student;
+        arrayIndex++;
     }
+
+    for (int i = 0; i < studentsCount; i++) {
+        printf("name: %10s | id: %5d | age: %4d | gender %3c\n", studentsArray[i].name, studentsArray[i].id, studentsArray[i].age, studentsArray[i].gender);
+    }
+
+    printf("\n\n COUNT: %d\n", studentsCount);
 
     fclose(pInputFile);
     fclose(pOutputFile);
