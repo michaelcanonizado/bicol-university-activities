14ebbf3844d1d83616e868c129563a75e9b319d0
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-03-17 22:25:25 +0800

    feat: add groupStudents() that groups the read students into male and female

--- 0103_computer-programming-2/pe-006/index.c
+++ 0103_computer-programming-2/pe-006/index.c
@@ -3,6 +3,30 @@
 #define MAX_NAME_LENGTH 100
 #define MAX_NUM_OF_STUDENTS 100
 
+
+typedef struct {
+    char name[MAX_NAME_LENGTH];
+    int id;
+    int age;
+    char gender;
+} Student;
+
+void groupStudents(Student allStudentsArray[MAX_NUM_OF_STUDENTS], int studentsCount, Student maleStudentsArray[MAX_NUM_OF_STUDENTS], int *maleStudentsCount, Student femaleStudentsArray[MAX_NUM_OF_STUDENTS], int *femaleStudentsCount) {
+
+    int m = 0, f = 0;
+
+    for (int i = 0; i < studentsCount; i++) {
+        if (allStudentsArray[i].gender == 'M' || allStudentsArray[i].gender == 'm') {
+            maleStudentsArray[m++] = allStudentsArray[i];
+        } else if (allStudentsArray[i].gender == 'F' || allStudentsArray[i].gender == 'f') {
+            femaleStudentsArray[f++] = allStudentsArray[i];
+        }
+    }
+
+    *maleStudentsCount = m;
+    *femaleStudentsCount = f;
+}
+
 int main(int argc, char *argv[]) {
     
     if (argc < 3) {
@@ -15,17 +39,13 @@ int main(int argc, char *argv[]) {
     pInputFile = fopen(argv[1], "r");
     pOutputFile = fopen(argv[2], "w");
 
-    typedef struct {
-        char name[MAX_NAME_LENGTH];
-        int id;
-        int age;
-        char gender;
-    } Student;
 
-    int studentsCount = 0, arrayIndex = 0;
-    Student studentsArray[MAX_NUM_OF_STUDENTS];
+    int allStudentsCount = 0, maleStudentsCount = 0, femaleStudentsCount = 0, arrayIndex = 0;
+    Student allStudentsArray[MAX_NUM_OF_STUDENTS];
+    Student maleStudentsArray[MAX_NUM_OF_STUDENTS];
+    Student femaleStudentsArray[MAX_NUM_OF_STUDENTS];
 
-    fscanf(pInputFile, "%d", &studentsCount);
+    fscanf(pInputFile, "%d", &allStudentsCount);
 
     while(1) {
         Student student;
@@ -34,15 +54,22 @@ int main(int argc, char *argv[]) {
             break;
         }
 
-        studentsArray[arrayIndex] = student;
+        allStudentsArray[arrayIndex] = student;
         arrayIndex++;
     }
 
-    for (int i = 0; i < studentsCount; i++) {
-        printf("name: %10s | id: %5d | age: %4d | gender %3c\n", studentsArray[i].name, studentsArray[i].id, studentsArray[i].age, studentsArray[i].gender);
+    groupStudents(allStudentsArray, allStudentsCount, maleStudentsArray, &maleStudentsCount, femaleStudentsArray, &femaleStudentsCount);
+
+    printf("\nMALE STUDENTS:\n");
+    for (int i = 0; i < maleStudentsCount; i++) {
+        printf("name: %10s | id: %5d | age: %4d | gender %3c\n", maleStudentsArray[i].name, maleStudentsArray[i].id, maleStudentsArray[i].age, maleStudentsArray[i].gender);
+    }
+    printf("\n\nFEMALE STUDENTS:\n");
+    for (int i = 0; i < femaleStudentsCount; i++) {
+        printf("name: %10s | id: %5d | age: %4d | gender %3c\n", femaleStudentsArray[i].name, femaleStudentsArray[i].id, femaleStudentsArray[i].age, femaleStudentsArray[i].gender);
     }
 
-    printf("\n\n COUNT: %d\n", studentsCount);
+    printf("\n\n COUNT: %d\n", allStudentsCount);
 
     fclose(pInputFile);
     fclose(pOutputFile);
