e897ecdea90fd677ab379ed3e0318102f3e28361
unknown <michaelxaviercanonizado@gmail.com>
2024-09-30 16:00:05 +0800

    feat(108): add ./DecodeMyPIN main algorithm

--- 0108_object-oriented-programming/DecodeMyPIN/src/main/java/org/example/Main.java
+++ 0108_object-oriented-programming/DecodeMyPIN/src/main/java/org/example/Main.java
@@ -12,8 +12,24 @@ public class Main {
         String encodedPIN;
 
         while(scanner.hasNext()) {
+            // Read PIN from input.in
             encodedPIN = scanner.nextLine();
-            System.out.println(encodedPIN);
+            // Remove '-' separating the characters
+            encodedPIN = encodedPIN.replace("-","");
+
+            // Iterate through the string and replace the necessary characters
+            for (int i = 0; i < encodedPIN.length(); i++) {
+                // If character can be parsed through Integer.parseInt(), convert it to integer and output it.
+                try {
+                    System.out.print("[" + Integer.parseInt(String.valueOf(encodedPIN.charAt(i))) + "]");
+                }
+                // Else if the current character can't the parsed, offset the ASCII value then output it.
+                catch (NumberFormatException e) {
+                    System.out.print("[" + ((int)encodedPIN.charAt(i) - 55) + "]");
+                }
+
+            }
+            System.out.println();
         }
     }
 }
