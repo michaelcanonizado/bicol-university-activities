4e8fc464256719dd729934dc13343af2334d4a1d
unknown <michaelxaviercanonizado@gmail.com>
2024-09-30 09:18:29 +0800

    feat(108): add FormatAuthorName rules and instructions

--- 0108_object-oriented-programming/FormatAuthorName/src/main/java/org/FormatAuthorName/Main.java
+++ 0108_object-oriented-programming/FormatAuthorName/src/main/java/org/FormatAuthorName/Main.java
@@ -2,6 +2,14 @@ package org.FormatAuthorName;
 
 public class Main {
     public static void main(String[] args) {
+        // Minimum of 1 part: last name
+        // Maximum of 3 parts: First, Middle, Last
+        // Middle name is optional
+
+        // Only the first letter of eahc part should be capital
+        // First and Second names should be the initials followed by a '.': E.g: lady agusTa -> L. Agusta
+        // Each character of the name should be a lower or upper case. No other numerical characters or symbols.
+        
         System.out.println("Hello world!");
     }
 }
