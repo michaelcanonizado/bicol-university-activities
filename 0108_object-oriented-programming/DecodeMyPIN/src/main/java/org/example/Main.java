0aa73f01508d49fc650bc3c44912b88d838bf0f0
unknown <michaelxaviercanonizado@gmail.com>
2024-09-30 14:54:40 +0800

    feat(108): read input.in and output PIN

--- 0108_object-oriented-programming/DecodeMyPIN/src/main/java/org/example/Main.java
+++ 0108_object-oriented-programming/DecodeMyPIN/src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = new File("input.in");
+        Scanner scanner = new Scanner(inputFile);
+
+        String encodedPIN;
+
+        while(scanner.hasNext()) {
+            encodedPIN = scanner.nextLine();
+            System.out.println(encodedPIN);
+        }
     }
 }
