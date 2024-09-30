bfee037ab762dad3c9edac9f102491d592cdf8a6
unknown <michaelxaviercanonizado@gmail.com>
2024-09-30 12:40:02 +0800

    feat(108): scan input.in author names

--- 0108_object-oriented-programming/FormatAuthorName/src/main/java/org/FormatAuthorName/Main.java
+++ 0108_object-oriented-programming/FormatAuthorName/src/main/java/org/FormatAuthorName/Main.java
@@ -1,7 +1,11 @@
 package org.FormatAuthorName;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // Minimum of 1 part: last name
         // Maximum of 3 parts: First, Middle, Last
         // Middle name is optional
@@ -9,7 +13,15 @@ public class Main {
         // Only the first letter of eahc part should be capital
         // First and Second names should be the initials followed by a '.': E.g: lady agusTa -> L. Agusta
         // Each character of the name should be a lower or upper case. No other numerical characters or symbols.
-        
-        System.out.println("Hello world!");
+
+        File inputFile = new File("input.in");
+        Scanner scanner = new Scanner(inputFile);
+
+        String name;
+
+        while(scanner.hasNext()) {
+            name = scanner.nextLine();
+            System.out.println("Name: "+name);
+        }
     }
 }
