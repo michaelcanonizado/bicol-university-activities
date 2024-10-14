61fa4bbbd43c5a59464733be0c0aa190c89e8aa4
unknown <michaelxaviercanonizado@gmail.com>
2024-10-14 22:31:46 +0800

    feat(108/PalindromeAB): add version 1 algorithm

--- 0108_object-oriented-programming/PalindromeAB/src/main/java/org/PalindromeAB/Main.java
+++ 0108_object-oriented-programming/PalindromeAB/src/main/java/org/PalindromeAB/Main.java
@@ -9,16 +9,60 @@ public class Main {
         File inputFile = new File("input.in");
         Scanner scanner = new Scanner(inputFile);
 
+        // get number of tests
         int numOfTests = Integer.parseInt(scanner.nextLine().trim());
-        System.out.println(numOfTests);
+
         for (int i = 0; i < numOfTests; i++) {
             String line = scanner.nextLine();
             String[] numbers = line.split(" ");
+
             int x = Integer.parseInt(numbers[0]);
             int y = Integer.parseInt(numbers[1]);
+            boolean isXEven = x % 2 == 0;
+            boolean isYEven = y % 2 == 0;
 
-            System.out.printf("x: %d, y: %d\n", x, y);
-//            System.out.println(line);
+            // Invalid Pair
+            if (isXEven == false && isYEven == false) {
+                System.out.println("-1");
+                continue;
+            }
+
+//            System.out.print(x + " " + y + ": ");
+
+            char firstChar = !isXEven && isYEven ? 'b' : 'a';
+            char secondChar = !isXEven && isYEven ? 'a' : 'b';
+            int firstCount = !isXEven && isYEven ? y : x;
+            int secondCount = !isXEven && isYEven ? x : y;
+            for (int j = 1; j <= 2; j++) {
+
+                for (int k = 0; k < firstCount/2; k++) {
+                    System.out.print(firstChar);
+                }
+                for (int k = 0; k < secondCount/2; k++) {
+                    System.out.print(secondChar);
+                }
+                if (!isXEven) {
+                    System.out.print("a");
+                }
+                if (!isYEven) {
+                    System.out.print("b");
+                }
+                for (int k = 0; k < secondCount/2; k++) {
+                    System.out.print(secondChar);
+                }
+                for (int k = 0; k < firstCount/2; k++) {
+                    System.out.print(firstChar);
+                }
+
+                System.out.println("");
+                char tempChar = firstChar;
+                firstChar = secondChar;
+                secondChar = tempChar;
+
+                int tempCount = firstCount;
+                firstCount = secondCount;
+                secondCount = tempCount;
+            }
         }
 
     }
