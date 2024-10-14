e27dac65fcf7b4409baf5c9f3fc880d2d8451da9
unknown <michaelxaviercanonizado@gmail.com>
2024-10-14 21:38:58 +0800

    feat(108/PalindromeAB): load x and y from input.in

--- 0108_object-oriented-programming/PalindromeAB/src/main/java/org/PalindromeAB/Main.java
+++ 0108_object-oriented-programming/PalindromeAB/src/main/java/org/PalindromeAB/Main.java
@@ -1,7 +1,25 @@
 package org.PalindromeAB;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = new File("input.in");
+        Scanner scanner = new Scanner(inputFile);
+
+        int numOfTests = Integer.parseInt(scanner.nextLine().trim());
+        System.out.println(numOfTests);
+        for (int i = 0; i < numOfTests; i++) {
+            String line = scanner.nextLine();
+            String[] numbers = line.split(" ");
+            int x = Integer.parseInt(numbers[0]);
+            int y = Integer.parseInt(numbers[1]);
+
+            System.out.printf("x: %d, y: %d\n", x, y);
+//            System.out.println(line);
+        }
+
     }
 }
