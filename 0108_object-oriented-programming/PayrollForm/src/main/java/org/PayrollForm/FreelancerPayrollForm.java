8bd4b0f9cfeec3d3033fe758fa54c2d106f1421a
unknown <michaelxaviercanonizado@gmail.com>
2024-11-09 13:25:48 +0800

    feat(108/PayrollForm): add FreelancerPayrollForm constructor

--- 0108_object-oriented-programming/PayrollForm/src/main/java/org/PayrollForm/FreelancerPayrollForm.java
+++ 0108_object-oriented-programming/PayrollForm/src/main/java/org/PayrollForm/FreelancerPayrollForm.java
@@ -1,4 +1,17 @@
 package org.PayrollForm;
 
 public class FreelancerPayrollForm {
+    private String freelancerName;
+    private double hourlyRate;
+    private double hoursWorked;
+    private double grossIncome;
+
+    public FreelancerPayrollForm (String freelancerName, double hourlyRate) {
+        this.freelancerName = freelancerName;
+        this.hourlyRate = hourlyRate;
+
+        this.hoursWorked = 0.0;
+        this.grossIncome = 0.0;
+    }
+
 }
