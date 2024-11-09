744ea4f9b54db92cb06f108c296e0067bf70c9bf
unknown <michaelxaviercanonizado@gmail.com>
2024-11-09 13:51:33 +0800

    docs(108/PayrollForm): add comments to FreelancerPayrollForm methods

--- 0108_object-oriented-programming/PayrollForm/src/main/java/org/PayrollForm/FreelancerPayrollForm.java
+++ 0108_object-oriented-programming/PayrollForm/src/main/java/org/PayrollForm/FreelancerPayrollForm.java
@@ -1,11 +1,13 @@
 package org.PayrollForm;
 
 public class FreelancerPayrollForm {
+    // Attributes
     private String freelancerName;
     private double hourlyRate;
     private double hoursWorked;
     private double grossIncome;
 
+    // Constructor
     public FreelancerPayrollForm (String freelancerName, double hourlyRate) {
         this.freelancerName = freelancerName;
         this.hourlyRate = hourlyRate;
@@ -14,10 +16,10 @@ public class FreelancerPayrollForm {
         this.grossIncome = 0.0;
     }
 
+    // Utility Methods
     public double calculateProjectPayment(double hoursWorked) {
         return this.hoursWorked * hourlyRate;
     }
-
     public void trackProjectPayment(double hoursWorked) {
         double payment = calculateProjectPayment((hoursWorked));
         this.grossIncome += payment;
@@ -27,7 +29,6 @@ public class FreelancerPayrollForm {
         System.out.println("Updated Hours Worked: " + this.hoursWorked);
         System.out.println("----------------------------------");
     }
-
     public void displayFreelancerInfo() {
         System.out.println("Freelancer Information:");
         System.out.println("Name: " + this.freelancerName);
@@ -37,18 +38,18 @@ public class FreelancerPayrollForm {
         System.out.println("----------------------------------");
     }
 
+    // Accessor Methods
     public String getFreelancerName() {
         return freelancerName;
     }
-
     public double getHourlyRate() {
         return hourlyRate;
     }
 
+    // Mutator Methods
     public void setFreelancerName(String freelancerName) {
         this.freelancerName = freelancerName;
     }
-
     public void setHourlyRate(double hourlyRate) {
         this.hourlyRate = hourlyRate;
     }
