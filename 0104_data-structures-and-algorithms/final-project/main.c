fcd0370b1421ed55e4dcd455e2d260e5812efbf9
unknown <michaelxaviercanonizado@gmail.com>
2024-11-17 22:12:51 +0800

    feat(q04/final-project): add main menu CLI

--- 0104_data-structures-and-algorithms/final-project/main.c
+++ 0104_data-structures-and-algorithms/final-project/main.c
@@ -2,7 +2,30 @@
 #include <stdlib.h>
 
 int main(void) {
-    printf("Hello World!");
+    
+    int selectedOption = 0;
+    char *menuOptions[] = {
+        "Linear Data Structures", 
+        "Non-Linear Data Structures", 
+        "Strings", 
+        "?",
+        "Exit"
+    };
+    int menuOptionsSize = sizeof(menuOptions)/sizeof(menuOptions[0]);
+
+    do {
+        for (int i = 0; i < menuOptionsSize; i++) {
+            printf("\n%d) %s", i+1, menuOptions[i]);
+        }
+        printf("\nPlease choose an option: ");
+        scanf("%d", &selectedOption);
+
+        printf("Selected: %d", selectedOption);
+
+    } while(selectedOption <= 0 || selectedOption > menuOptionsSize);
 
     return 0;
 }
+
+
+
