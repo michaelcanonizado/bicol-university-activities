587c3d131c0b7be01ee15b68cec90a9d019172b4
unknown <michaelxaviercanonizado@gmail.com>
2024-11-18 19:53:35 +0800

    feat(104/final-project): add getUserIntegerInputs() temporary commit

--- 0104_data-structures-and-algorithms/final-project/main.c
+++ 0104_data-structures-and-algorithms/final-project/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 
+typedef struct AllocatedArraysNode {
+    int *value;
+    size_t size;
+    struct AllocatedArraysNode *next;
+} AllocatedArraysNode;
+AllocatedArraysNode *allocatedArraysHead = NULL;
+
 int displayOptions(char *menuOptions[], int menuOptionsSize, char *header);
+void getUserIntegerInputs(void);
 
 void linearDataStructuresMenu(void);
 void searchingMenu(void);
@@ -14,6 +24,8 @@ void linkedListsDeleteMenu(void);
 void linkedListsSearchMenu(void);
 void linkedListsReverseMenu(void);
 void linkedListsMergeMenu(void);
+
+void linearSearchMenu(void);
 
 int main(void) {
     char *menuOptions[] = {
@@ -61,7 +73,50 @@ int displayOptions(char *menuOptions[], int menuOptionsSize, char *header) {
 
     return selectedOption;
 }
+int isValidInteger(const char *str) {
+    // Check if the string is empty
+    if (*str == '\0') {
+        return 0;
+    }
 
+    // Allow for an optional leading negative sign
+    if (*str == '-') {
+        str++; // Skip the negative sign
+    }
+
+    // Ensure all characters are digits
+    while (*str) {
+        if (!isdigit(*str)) {
+            return 0; // Return 0 if any non-digit character is found
+        }
+        str++;
+    }
+    
+    return 1;
+}
+void getUserIntegerInputs(void) {
+    int array[5] = {0};
+    char inputStr[100];
+    const char delimiter[] = ","; 
+    char *token;
+    int numOfInputs = 0;
+    int inputStrLength = 0;
+
+    printf("\nPlease enter your inputs(integers): ");
+    scanf("%s",inputStr);
+
+    while (inputStr[inputStrLength] != '\0') {
+        if ()
+        inputStrLength++;
+    }
+
+    token = strtok(inputsStr, delimiter);
+
+    while (token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, delimiter);
+    }
+}
 
 
 
@@ -102,6 +157,8 @@ void searchingMenu(void) {
     int selectedOption = displayOptions(menuOptions, menuOptionsSize, "Main Menu > Searching");
 
     switch (selectedOption) {
+        case 1:
+            linearSearchMenu();
         case 5:
             main();
         default:
@@ -234,4 +291,8 @@ void linkedListsMergeMenu(void) {
     int selectedOption = displayOptions(menuOptions, menuOptionsSize, "Main Menu > Linear Data Structures > Linked Lists > Merge");
 
     printf("Selected: %d", selectedOption);
+}
+
+void linearSearchMenu(void) {
+    getUserIntegerInputs();
 }
