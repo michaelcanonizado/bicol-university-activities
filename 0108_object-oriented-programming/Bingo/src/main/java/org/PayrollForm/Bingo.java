fab11d252837a20f8abc7f0b0fab10370885a6d9
unknown <michaelxaviercanonizado@gmail.com>
2024-11-22 20:48:24 +0800

    refactor(108/Bingo): rename col to row, and row to col

--- 0108_object-oriented-programming/Bingo/src/main/java/org/PayrollForm/Bingo.java
+++ 0108_object-oriented-programming/Bingo/src/main/java/org/PayrollForm/Bingo.java
@@ -7,26 +7,30 @@ public class Bingo {
     private final int SIZE = 5;
     private final int MAX_NUMBER = 75;
     private final Random RANDOM = new Random();
-    private int[][] bingoCard;
+    private int[][] card;
 
-    private int[][] generateBingoCard() {
+    public Bingo() {
+        this.card = generateCard();
+    }
+
+    private int[][] generateCard() {
         int[][] card = new int[SIZE][SIZE];
 
-        for (int col = 0; col < SIZE; col++) {
-            int min = (col * 15) + 1;
+        for (int row = 0; row < SIZE; row++) {
+            int min = (row * 15) + 1;
             int max = min + 14;
             HashSet<Integer> generatedNumsInCol = new HashSet<>();
 
-            for (int row = 0; row < SIZE; row++) {
-                if (row == 2 && col == 2) {
-                    card[row][col] = 0;
+            for (int col = 0; col < SIZE; col++) {
+                if (col == (SIZE / 2) && row == (SIZE / 2)) {
+                    card[col][row] = 0;
                 } else {
                     int number;
                     do {
                         number = RANDOM.nextInt(max - min + 1) + min;
                     } while (generatedNumsInCol.contains(number));
                     generatedNumsInCol.add(number);
-                    card[row][col] = number;
+                    card[col][row] = number;
                 }
             }
 
