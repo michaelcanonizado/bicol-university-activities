2977d6f26096feac5696dcd84dd0440cda14070e
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-05-02 00:23:24 +0800

    chore(105): add .sql backup

--- 105_information-management/final-project/seeder/seeder.js
+++ 105_information-management/final-project/seeder/seeder.js
@@ -1,23 +1,29 @@
 const mysql = require('mysql2/promise');
 const { faker } = require('@faker-js/faker');
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const results = [];
 
 const INSERT_COUNT = 50;
 
-async function seedDatabase() {
-	const connection = await mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: '',
-		database: 'iflair-dental-clinic-management-system',
-	});
+// async function seedDatabase() {
+// 	const connection = await mysql.createConnection({
+// 		host: 'localhost',
+// 		user: 'root',
+// 		password: '',
+// 		database: 'iflair-dental-clinic-management-system',
+// 	});
 
-	console.log('Connected to MySQL.');
+// 	console.log('Connected to MySQL.');
 
-	const [rows] = await connection.execute('SELECT * FROM employee LIMIT 10');
-	console.log(rows);
+// 	const [rows] = await connection.execute('SELECT * FROM employee LIMIT 10');
+// 	console.log(rows);
 
-	console.log(`${INSERT_COUNT} fake records inserted.`);
-	await connection.end();
-}
+// 	console.log(`${INSERT_COUNT} fake records inserted.`);
+// 	await connection.end();
+// }
 
-seedDatabase().catch(console.error);
+// seedDatabase().catch(console.error);
+
+
