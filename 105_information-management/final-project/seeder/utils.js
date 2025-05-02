6d676dd8c9bda56436e5a963bb388388831e032b
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-05-02 18:10:27 +0800

    feat(105): add calculate_age()

--- 105_information-management/final-project/seeder/utils.js
+++ 105_information-management/final-project/seeder/utils.js
@@ -1,3 +1,20 @@
 export function get_random_element(array) {
 	return array[Math.floor(Math.random() * array.length)];
 }
+
+export function calculate_age(birthDate) {
+	const today = new Date();
+	const birth = new Date(birthDate);
+
+	let age = today.getFullYear() - birth.getFullYear();
+	const has_birthday_this_year =
+		today.getMonth() > birth.getMonth() ||
+		(today.getMonth() === birth.getMonth() &&
+			today.getDate() >= birth.getDate());
+
+	if (!has_birthday_this_year) {
+		age--;
+	}
+
+	return age;
+}
