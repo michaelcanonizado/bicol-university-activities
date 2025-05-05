de348304b2b9673b3ceba4948c286691bfc52e31
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-05-05 14:37:04 +0800

    chore(112) initialize cobol sub folder

---
+++ 112_programming-languages/cobol/hello-world/hello-world.cob
@@ -0,0 +1,7 @@
+000100 IDENTIFICATION DIVISION.                                         000100
+000150 PROGRAM-ID. HELLO-WORLD.                                         000101
+000250* THIS IS A COMMENT LINE                                          000102
+000300 PROCEDURE DIVISION.                                              000103
+000350 A000-FIRST-PARA.                                                 000104
+000400     DISPLAY "Hello World!".                                      000105
+000500     STOP RUN.                                                    000106
