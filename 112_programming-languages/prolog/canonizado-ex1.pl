624a4f87fd6d8c82f62260d10908d52f1e999471
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-05-19 09:46:53 +0800

    feat(112): add prolog activieis

--- 112_programming-languages/prolog/canonizado-ex1.pl
+++ 112_programming-languages/prolog/canonizado-ex1.pl
@@ -1,3 +1,24 @@
-student_in_computer_science(alden).
-student_in_college_of_science(X) :-                   
-    student_in_computer_science(X).
+student(alden, cs, 20).
+student(sophia, chem, 18).
+student(jacob, ce, 17).
+
+college(cs, science).
+college(it, science).
+college(bio, science).
+college(chem, science).
+college(met, science).
+college(ce, engineering).
+
+student_in_college(Student, Department, _) :- 
+    student(Student, Department, _).
+
+student_in_college(Student, College, _) :-
+    student(Student, Department, _),
+    college(Department, College).
+
+student_in_college_of_science(Student) :-
+    student_in_college(Student, science, _).
+
+pwedeng_uminom_na_student(Name, Course, Age) :-
+    student(Name, Course, Age),
+    Age >= 18.
---
+++ 112_programming-languages/prolog/test.pl
@@ -0,0 +1,5 @@
+college(cs, science);
+college(it, science);
+college(bio, science);
+college(chem, science);
+college(met, science);
